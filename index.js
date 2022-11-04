398e185d9b022fbcf82b868d90addccbeadd34ee
Abu Rayhan Kabir <aburayhankabir73@gmail.com>
2022-11-04 02:09:55 -0700

    firstCommit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require("dotenv")
+const cors = require("cors")
+dotenv.config()
+const PORT = process.env.PORT || 8000;
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = "mongodb+srv://Person:rPfGvIfRjbAu2TXk@cluster0.iuinxk2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run()
+{
+    try {
+        await client.connect();
+        const productCollection = client.db("Products").collection("Product");
+        app.get('/products', async(req, res)=>{
+            console.log("Query", req.query)
+            const page = parseInt( req.query.page);
+            const size = parseInt( req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if(page){
+                products = await cursor.skip(page).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray()
+            }
+            res.send(products)
+        })
+        app.get('/productCount', async(req, res)=>{
+            const query ={};
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({count})
+        })
+
+    } finally{
+        //await client.close();
+    }
+}
+run().catch(console.dir)
+app.listen(PORT , ()=>{
+    console.log("Successfully server start at ", PORT)
+})
+
+
